3e1b0e03ae108bdc3afc544b47efaa29d84a0e8d
megukaphii <40730335+megukaphii@users.noreply.github.com>
2024-04-10 14:27:34 +1000

    Initial Commit

---
+++ VersionSample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VersionSample;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ VersionSample/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace VersionSample;
+
+public partial class MainPage : ContentPage
+{
+    private readonly string _version = AppInfo.Version.ToString();
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        VersionLabel.Text = _version;
+    }
+}
